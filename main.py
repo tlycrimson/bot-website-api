32269cb684e4d802fe0febbea2a4b8358bf459bd
Ade A <papacr1msonn@gmail.com>
2025-12-22 20:02:18 +0000

    Fix CORS for frontend

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from supabase_client import supabase
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
